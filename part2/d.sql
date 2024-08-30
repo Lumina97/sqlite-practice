0ce980172f01022d11475c8d12180f71fe8e14a2
Lumina <erik04.nivala22@gmail.com>
2024-08-30 18:52:10 -0500

    Solved

--- part2/d.sql
+++ part2/d.sql
@@ -1,4 +1,6 @@
 -- Below is a dummy query, if we leave it empty then the SQLITE package will throw a non-readable error
 -- Change the query to pass the test :)
-select *
-from dogs;
+SELECT first_name , last_name 
+FROM Favorites 
+JOIN Users ON  Favorites.user_id = users.id
+WHERE dog_id = 2;
