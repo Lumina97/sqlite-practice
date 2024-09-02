faa2b32db7941e4b8987f968c3b2d889f8f2ec74
Lumina <erik04.nivala22@gmail.com>
2024-09-02 11:49:57 -0500

    Fixed formatting using prettier SQL and fixed excercise 2D to now use the dogs name in the query instead of ID

--- part1/scrap.sql
+++ part1/scrap.sql
@@ -1,50 +1,49 @@
-CREATE TABLE Users(
-  id INTEGER PRIMARY KEY ,
-  first_name TEXT NOT NULL,
-  last_name TEXT NOT NULL,
-  motto TEXT
-);
-INSERT INTO Users (first_name,last_name,motto) 
-VALUES ('Jon','Higgz','I love coding');
+DROP TABLE Dogs;
 
-INSERT INTO Users (first_name,last_name,motto)
- VALUES ('Andrey','Rusterton','I love coding even more');
+DROP TABLE Users;
 
-INSERT INTO Users (first_name,last_name,motto)
- VALUES ('Peter','Garboni','I love coding even more');
+DROP TABLE Favorites;
 
+CREATE TABLE
+  Users (
+    id INTEGER PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    motto TEXT
+  );
 
- CREATE TABLE Dogs(id INTEGER PRIMARY KEY, name TEXT NOT NULL);
- INSERT INTO Dogs (name)
- VALUES ('DOOMSLAYER');
- 
- INSERT INTO Dogs (name)
- VALUES ('Zoey');
- 
- INSERT INTO Dogs (name)
- VALUES ('Jefferey');
+INSERT INTO
+  Users (first_name, last_name, motto)
+VALUES
+  ('Jon', 'Higgz', 'I love coding'),
+  ('Andrey', 'Rusterton', 'I love coding even more'),
+  ('Peter', 'Garboni', 'I love coding even more');
 
+CREATE TABLE
+  Dogs (id INTEGER PRIMARY KEY, name TEXT NOT NULL);
 
-CREATE TABLE Favorites(
-  id INTEGER PRIMARY KEY,
-  user_id INTEGER NOT NULL,
-  dog_id INTEGER NOT NULL,
-  UNIQUE(user_id,dog_id),
-  FOREIGN KEY(dog_id) REFERENCES dogs(id),
-  FOREIGN KEY(user_id) REFERENCES users(id)
-);
+INSERT INTO
+  Dogs (name)
+VALUES
+  ('DOOMSLAYER'),
+  ('Zoey'),
+  ('Jefferey');
 
-INSERT INTO Favorites (user_id,dog_id) 
-VALUES (3,1);
+CREATE TABLE
+  Favorites (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    dog_id INTEGER NOT NULL,
+    UNIQUE (user_id, dog_id),
+    FOREIGN KEY (dog_id) REFERENCES dogs (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+  );
 
-INSERT INTO Favorites (user_id,dog_id) 
-VALUES (1,1);
-
-INSERT INTO Favorites (user_id,dog_id) 
-VALUES (2,1);
-
-INSERT INTO Favorites (user_id,dog_id) 
-VALUES (2,2);
-
-INSERT INTO Favorites (user_id,dog_id) 
-VALUES (1,3);
+INSERT INTO
+  Favorites (user_id, dog_id)
+VALUES
+  (3, 1),
+  (1, 1),
+  (2, 1),
+  (2, 2),
+  (1, 3);
--- part2/d.sql
+++ part2/d.sql
@@ -1,6 +1,11 @@
 -- Below is a dummy query, if we leave it empty then the SQLITE package will throw a non-readable error
 -- Change the query to pass the test :)
-SELECT first_name , last_name 
-FROM Favorites 
-JOIN Users ON  Favorites.user_id = users.id
-WHERE dog_id = 2;
+SELECT
+  first_name,
+  last_name
+FROM
+  Favorites
+  JOIN Users ON Favorites.user_id = users.id
+  JOIN Dogs ON Favorites.dog_id = dogs.id
+WHERE
+  dogs.name = 'Zoey';
